410c4556f4b86847476d3d2e0560c2cc9083dd57
zhaohui <>
2024-09-29 09:58:04 +0800

    s

--- test.go
+++ test.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func test() {
-	5fmt.Println("1212")222
-	6fmt.Println("1212")1111111111111666
-	1fmt.Println("1212")1111111111111121221
-	8
+	fmt.Println("1212")
+	fmt.Println("1212")
+	fmt.Println("1212")
 }
